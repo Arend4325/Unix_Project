e62885819341d529c6d9fe91450878fc02cac1d9
Superdev514 <devenshahphan@gmail.com>
2025-11-27 10:02:23 -0500

    Added System & Network scripts

---
+++ Network.sh
@@ -0,0 +1,116 @@
+
+menu() {
+
+	echo ""
+	echo "==== NETWORK MANAGEMENT MENU ===="
+	echo "1) Display Network Interfaces, IP Adresses & Default Gateways"
+	echo "2) Enable or Disable a Network Interface"
+        echo "3) Assign IP address to Network Card"
+        echo "4) Display Wi-Fi networks"
+        echo "5) Exit"
+	echo "================================="
+
+}
+
+display_networkInterfaces() {
+	echo ""
+
+	echo "Network Interfaces and their IP Addresses: "
+	for interface in $(ls /sys/class/net/); do
+		ip_address=$(ip -o -4 addr show $interface | awk '{print $4}')
+		if [ -n "$ip_address" ]; then
+			echo "Interface: $interface, IP Address: $ip_address"
+		else
+			echo "Interface: $interface, No IP Address assigned"
+		fi
+	done
+
+	echo ""
+	echo "Default Gateways: "
+	ip route show default
+
+}
+
+
+enableOrDisable_Interface() {
+	echo ""
+	echo "Availabe Interfaces"
+	ip -brief addr show
+
+	echo ""
+
+	read -p "Enter an interface name: " interface
+
+	echo "1) Enable Interface"
+	echo "2) Disable Interface"
+	echo "3) Back"
+	read -p "Choose an option: " choice
+
+	case "$choice" in
+		1)
+			sudo ip link set "$interface" up
+			echo -e "$interface enabled.";;
+		2)
+			sudo ip link set "$interface" down
+			echo -e "$interface disabled.";;
+		3)
+			return;;
+		*)
+			echo "Invalid Option";;
+	esac
+
+}
+
+
+assign_IP() {
+	echo ""
+	echo "Available Interfaces"
+	ip -brief addr show
+
+	read -p "Choose an interface: " interface
+
+	read -p "Enter an IP address: " ip
+
+	sudo ip addr add "$ip" dev "$interface"
+
+}
+
+
+display_wifiNetworks() {
+	echo ""
+	echo "Available Wifi-Networks"
+
+	echo "testing vmare wifi"
+	if ! nmcli dev status | grep -qw wifi; then
+		echo -e "testing to see if wifi shows"
+		return
+	fi
+
+	nmcli device wifi list
+
+	echo ""
+
+	echo "Connect to Wi-Fi"
+	read -p "Enter SSID: " ssid
+	read -sp "Enter Password: " pass
+	echo ""
+	nmcli device wifi connect "$ssid" password "$pass"
+
+}
+
+
+
+
+while true; do
+	menu
+	read -p "Select an option: " opt
+
+	case $opt in
+		1) display_networkInterfaces;;
+		2) enableOrDisable_Interface;;
+		3) assign_IP;;
+		4) display_wifiNetworks;;
+		5) exit 0;;
+		*) echo "Invalid option";;
+	esac
+done
---
+++ System_Status.sh
@@ -0,0 +1,102 @@
+menu() {
+    echo ""
+    echo "===== SYSTEM STATUS MENU ====="
+    echo "1) Display Memory Usage"
+    echo "2) Check CPU Temp"
+    echo "3) List Active System Processes"
+    echo "4) Stop a process"
+    echo "5) Exit"
+    echo "=============================="
+}
+
+
+display_memory() {
+	echo ""
+	echo "Detailed Memory Usage"
+	free -m
+}
+
+
+cpu_temp() {
+	echo ""
+	echo "CPU Temperature"
+	cpuTemp=$(cat /sys/class/thermal/thermal_zone0/temp)
+	cpuTempC=$(echo "$cpuTemp / 1000" | bc -l)
+
+	echo "CPU Temperature:  $cpuTempC °C"
+
+	if (( $(echo "$cpuTempC > 70" | bc -l) )); then
+		echo "WARNING!!! CPU Temperature is greater than 70!°C"
+	fi
+}
+
+list_systems() {
+	echo ""
+	echo "Active Processes"
+	top |head -n 20
+
+}
+
+process_menu() {
+
+	echo ""
+	echo "===== PROCESS MENU ====="
+	echo "1) Stop  process"
+	echo "2) Terminate a process"
+	echo "3) Exit"
+
+}
+
+stop_process() {
+	while true; do
+		process_menu
+		read -p "Choose an option: " opt
+
+		case $opt in
+			1)
+				echo "============= ACTIVE PROCESSES ============="
+				printf "%-7s %-15s %-7s %-15s %-7s %-15s\n" \
+				       "PID" "COMMAND" "PID" "COMMAND" "PID" "COMMAND"
+				ps -u $USER -o pid:10,comm:25 --sort=pid --no-headers | column -t | paste - - - | column -t
+				echo "============================================"
+				echo ""
+
+				read -p "Enter the PID of the process to stop: " process
+
+				kill -SIGSTOP "$process";;
+
+
+			2)
+				echo "============= ACTIVE PROCESSES ============="
+                                printf "%-7s %-15s %-7s %-15s %-7s %-15s\n" \
+                                       "PID" "COMMAND" "PID" "COMMAND" "PID" "COMMAND"
+                                ps -u $USER -o pid:10,comm:25 --sort=pid --no-headers | column -t | paste - - - | column -t
+                                echo "============================================"
+                                echo ""
+
+
+                                read -p "Enter the PID of the process to terminate: " process
+
+                                kill -SIGTERM "$process";;
+
+			3)
+				break;;
+			*) echo "Invalid Option";;
+	        esac
+	done
+}
+
+
+while true; do
+    menu
+    read -p "Select an option: " opt
+
+    case $opt in
+        1) display_memory ;;
+        2) cpu_temp ;;
+        3) list_systems ;;
+        4) stop_process ;;
+        5) exit 0 ;;
+        *) echo "Invalid option" ;;
+    esac
+done
