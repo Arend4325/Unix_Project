d6e4f72694f9c6e36af41d0092d5b624c44a972d
nmadil <adilnaqibm@gmail.com>
2025-11-27 09:01:16 -0500

    FileSizeManagement

---
+++ filesCheck.sh
@@ -0,0 +1,37 @@
+PS3="Please enter a number to choose the option you'd like to proceed with: "
+select fileMgmt in findFile LargestFiles OldestFiles Exit
+do
+case $fileMgmt in
+findFile)
+read -p "Please enter username: " user
+read -p "Please enter file name: " file
+if id "$user" &>/dev/null; then
+echo "User '$user' exists."
+if [ -f "$file" ]
+then
+echo "$file has been found"
+else
+echo "$file does not exist!"
+fi
+else
+echo "$user does not exist!"
+fi
+;;
+LargestFiles)
+largest=$(du -a -h "$HOME" | sort -rh | head)
+echo "The 10 largest files are: "
+echo "$largest"
+;;
+OldestFiles)
+oldest=$(ls -ltr "$HOME" | head | awk '{print $6, $7, $8, $9}')
+echo "The 10 oldest files are: "
+echo "$oldest"
+;;
+Exit)
+echo "You have exited the program."
+break
+;;
+*)
+echo "Invalid option!"
+esac
+done
